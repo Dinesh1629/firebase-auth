7f1a169b1ed89274c1171d6f57e9a7b6f975f08e
DineshVelliyangiri <dineshpraba1629@gmail.com>
2021-02-08 17:17:33 +0530

    react with firebase authentication

---
+++ src/App.js
@@ -0,0 +1,34 @@
+// import React from "react";
+// import "./App.css";
+// // import { BrowserRouter as Router, Route } from "react-router-dom";
+// // import Home from "Home";
+// // import Login from "Login";
+// // import SignUp from "SignUp";
+// // import { AuthProvider } from "Auth";
+// // import PrivateRoute from "PrivateRoute";
+
+// const App = () => {
+//   return (
+    
+//         <div>
+//           <Home />
+         
+//         </div>
+     
+//   );
+// };
+
+// export default App;
+
+import './App.css';
+import Login from './Login';
+
+function App() {
+  return (
+    <div className="App">
+    <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Login.js
@@ -0,0 +1,46 @@
+import React, { useCallback, useContext } from "react";
+import login from './Login';
+import { withRouter, Redirect } from "react-router";
+import app from "./firebase";
+ import { AuthContext } from "./Auth";
+
+// const Login = ({ history }) => {
+//   const handleLogin = useCallback(
+//     async event => {
+//       event.preventDefault();
+//       const { email, password } = event.target.elements;
+//       try {
+//         await app
+//           .auth()
+//           .signInWithEmailAndPassword(email.value, password.value);
+//         history.push("/");
+//       } catch (error) {
+//         alert(error);
+//       }
+//     },
+//     [history]
+//   );
+
+//   const { currentUser } = useContext(AuthContext);
+
+//   if (currentUser) {
+//     return <Redirect to="/" />;
+//   }
+
+  
+    <div>
+      <h1>Log in</h1>
+      <form >
+        <label>
+          Email
+          <input name="email" type="email" placeholder="Email" />
+        </label>
+        <label>
+          Password
+          <input name="password" type="password" placeholder="Password" />
+        </label>
+        <button type="submit">Log in</button>
+      </form>
+    </div>
+  
+export default Login;
